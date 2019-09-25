aef29c81c2807b4843364fb1fae79aaf650f75d1
Divesh Gandhi <divesh.gandhi@gmail.com>
2019-09-25 19:52:45 +0530

    Adding upgrad phone dir solution as compressed folder

--- code examples/Course 7 React/New folder/phone-directory-master/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import './common/common.css';
-import PhoneDirectory from './PhoneDirectory';
-
-ReactDOM.render(<PhoneDirectory />, document.getElementById('root'));
-registerServiceWorker();
