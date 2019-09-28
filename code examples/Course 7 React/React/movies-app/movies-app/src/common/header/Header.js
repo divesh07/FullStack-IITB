3a4e6f3afb07b7ed7ecb2a9480f870550861331f
Divesh Gandhi <divesh.gandhi@gmail.com>
2019-09-29 00:44:43 +0530

    Added 'required' field validation in username control in login form

--- code examples/Course 7 React/React/movies-app/movies-app/src/common/header/Header.js
+++ code examples/Course 7 React/React/movies-app/movies-app/src/common/header/Header.js
@@ -10,6 +10,7 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import PropTypes from 'prop-types';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 const customStyles = {
     content: {
@@ -33,12 +34,15 @@ const TabContainer = function(props){
 TabContainer.propTypes = {
     children: PropTypes.node.isRequired
 }
+
 class Header extends Component {
     constructor(){
         super();
         this.state = {
             modalIsOpen: false,
-            value: 0
+            value: 0,
+            username: "",
+            usernameRequired: "dispNone"
         };
     }
 
@@ -52,6 +56,15 @@ class Header extends Component {
 
     tabChangeHandler = (event, value) => {
         this.setState({value})
+    }
+
+    loginClickHandler = () => {
+        this.state.username === "" ? this.setState({usernameRequired: "dispBlock"}) : 
+        this.setState({usernameRequired: "dispNone"});
+    }
+
+    inputUsernameChangeHandler = (e) => {
+        this.setState({username: e.target.value})
     }
 
     render(){
@@ -75,17 +88,22 @@ class Header extends Component {
                         </Tabs>
                         {this.state.value === 0 &&
                             <TabContainer id="tab-container">
+                                
                                 <FormControl required>
                                     <InputLabel htmlFor="username"> UserName</InputLabel>
-                                    <Input id="username" type="text" />
+                                    <Input id="username" type="text" username={this.state.username} onChange={this.inputUsernameChangeHandler}/>
+                                    <FormHelperText className={this.state.usernameRequired}>
+                                        <span className="red">required</span>
+                                    </FormHelperText>
                                 </FormControl>
                                 <br/><br/>
+
                                 <FormControl required>
                                     <InputLabel htmlFor="password"> Password</InputLabel>
                                     <Input id="password" type="password" />
                                 </FormControl>
                                 <br/><br/>
-                                <Button variant="contained" color="primary">Login
+                                <Button variant="contained" color="primary" onClick={this.loginClickHandler}>Login
                                 </Button>
                             </TabContainer>
                         }
