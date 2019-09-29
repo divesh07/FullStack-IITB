6db8712a94f83053b38887c787b14a61a91eb41a
Divesh Gandhi <divesh.gandhi@gmail.com>
2019-09-30 01:43:54 +0530

    Added poster, title and genres on details page

--- code examples/Course 7 React/React/movies-app/movies-app/src/screens/details/Details.js
+++ code examples/Course 7 React/React/movies-app/movies-app/src/screens/details/Details.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
+import { Typography } from '@material-ui/core';
+import './Details.css';
 
 class Details extends Component {
 
@@ -11,24 +13,38 @@ class Details extends Component {
         }
     }
 
-    componentDidMount(){
+    componentWillMount() {
         let currentState = this.state;
         currentState.movie = moviesData.filter((mov) => {
             return mov.id === this.props.movieId
         })[0];
 
-        this.setState({currentState});
+        this.setState({ currentState });
         console.log(this.state);
     }
 
     render() {
+        let movie = this.state.movie;
         return (
             <div className="details">
                 <Header />
                 <div className="flex-containerDetails">
                     <div className="leftDetails">
+                        <img src={movie.poster_url} alt={movie.title} />
                     </div>
                     <div className="middleDetails">
+                        <div>
+                            <Typography variant="headline" component="h2">
+                                {movie.title}
+                            </Typography>
+                        </div>
+
+                        <div>
+                            <Typography>
+                                <span className="bold">Genre: </span>
+                                {movie.genres.join(',')}
+                            </Typography>
+                        </div>
                     </div>
                     <div className="rightDetails">
                     </div>
