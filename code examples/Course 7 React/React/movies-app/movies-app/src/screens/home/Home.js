182f9bad3fd4a948545d9f376e3aaa9703ed719d
Divesh Gandhi <divesh.gandhi@gmail.com>
2019-09-29 22:36:13 +0530

    Added 'Movie Name' filter on home page

--- code examples/Course 7 React/React/movies-app/movies-app/src/screens/home/Home.js
+++ code examples/Course 7 React/React/movies-app/movies-app/src/screens/home/Home.js
@@ -22,6 +22,10 @@ const styles = theme => ({
         flexWrap: 'nowrap',
         transform: 'translateZ(0)',
         width: '100%'
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
     }
 });
 
@@ -43,6 +47,28 @@ class Home extends Component {
                         </GridListTile>
                     ))}
                 </GridList>
+                
+                <div className="flex-container">
+                    <div className="left">
+                        <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
+                        {
+                            moviesData.map(
+                                movie => (
+                                    <GridListTile className="released-movie-grid-item" key={"gird" + movie.id}>
+                                        <img src={movie.poster_url} className="movie-poster" alt={movie.title} ></img>
+                                        <GridListTileBar>
+                                            title={movie.title}
+                                            subtitle = { <span>Release Date: {new Date(movie.release_date).toDateString() }</span>}
+                                        </GridListTileBar>
+                                    </GridListTile>
+                                )
+                            )
+                        }
+                        </GridList>
+
+                    </div>
+
+                </div>
             </div>
         );
     }
